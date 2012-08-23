76746a2a556739920638f737f1387142028ac175
Accalia de Elementia <Accalia.de.Elementia@gmail.com>
2012-08-23 09:34:25 -0400

    Fix JSLINT for euler 1. Add optimized solution for Euler 2

--- JavaScript/euler1.js
+++ JavaScript/euler1.js
@@ -4,16 +4,16 @@ print('I will try the naive solution first.');
 
 var time = 0,
     elapsed = 0,
-    sum = 0, 
+    sum = 0,
     i = 0;
 
-time = (new Date).getTime();
+time = (new Date()).getTime();
 for (i = 0; i < 1000; i = i + 1) {
     if (i % 3 === 0 || i % 5 === 0) {
         sum = sum + i;
     }
 }
-elapsed = (new Date).getTime() - time;
+elapsed = (new Date()).getTime() - time;
 
 print('I think the answer is: ' + sum);
 print('I thought about it for ' + elapsed + ' miliseconds.');
@@ -26,9 +26,9 @@ function sumDivBy(target, by) {
     return (by * p * (p + 1)) / 2;
 }
 
-time = (new Date).getTime();
+time = (new Date()).getTime();
 sum = sumDivBy(999, 3) + sumDivBy(999, 5) - sumDivBy(999, 15);
-elapsed = (new Date).getTime() - time;
+elapsed = (new Date()).getTime() - time;
 
 print('I think the answer is: ' + sum);
 print('I thought about it for ' + elapsed + ' miliseconds.');
--- JavaScript/euler2.js
+++ JavaScript/euler2.js
@@ -1,17 +1,41 @@
 print('Find the sum of the even fibonacci numbers that do not exceed four million.');
 
 var sum = 0,
-    fiba = 1,
-    fibb = 1,
-    fibc = 0;
+    a = 1,
+    b = 1,
+    c = a + b,
+    time;
 
-while (fiba < 4000000) {
-    if (fiba % 2 === 0) {
-        sum = sum + fiba;
+print('');
+print('I will try the naive solution first.');
+time = (new Date()).getTime();
+while (c < 4000000) {
+    if (a % 2 === 0) {
+        sum = sum + a;
     }
-    fibc = fibb;
-    fibb = fiba;
-    fiba = fibb + fibc;
+    c = b;
+    b = a;
+    a = b + c;
 }
+time = (new Date()).getTime() - time;
+print('I think the answer is: ' + sum);
+print('I thought about it for ' + time + ' milliseconds.');
+print('');
+print('I will try to calculate without requiring division.');
 
-print('i think the asnwer is: ' + sum);
+sum = 0;
+a = 1;
+b = 1;
+c = a + b;
+
+time = (new Date()).getTime();
+while (c < 4000000) {
+    sum += c;
+    a = b + c;
+    b = c + a;
+    c = a + b;
+}
+time = (new Date()).getTime() - time;
+print('I think the answer is: ' + sum);
+print('I thought about it for ' + time + ' milliseconds.');
+
